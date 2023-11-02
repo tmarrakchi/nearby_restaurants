f0f4afabc672acc48f5fb46a9c428c79857236ab
M06088 <M06088@bpifrance.fr>
2023-11-02 19:01:24 +0100

    refator app : OOB

---
+++ src/point.py
@@ -0,0 +1,5 @@
+# Define a Point class to represent a geographical point
+class Point:
+    def __init__(self, latitude, longitude):
+        self.latitude = latitude
+        self.longitude = longitude
---
+++ src/restaurant.py
@@ -0,0 +1,7 @@
+# Define a Restaurant class to represent restaurant locations
+class Restaurant:
+    def __init__(self, name, latitude, longitude, distance):
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+        self.distance = distance
